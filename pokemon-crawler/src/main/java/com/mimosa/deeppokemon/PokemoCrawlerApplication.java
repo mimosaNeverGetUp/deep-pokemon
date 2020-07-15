f55fa01b683bfdb995169b4c8f5df1555339c904
mimosa <779032284@qq,com>
2020-07-16 01:14:13 +0800

    ui promote

--- pokemon-crawler/src/main/java/com/mimosa/deeppokemon/PokemoCrawlerApplication.java
+++ pokemon-crawler/src/main/java/com/mimosa/deeppokemon/PokemoCrawlerApplication.java
@@ -1,16 +1,27 @@
 package com.mimosa.deeppokemon;
 
+import com.mimosa.deeppokemon.config.ScheduledConfig;
 import com.mimosa.deeppokemon.crawler.LadderBattleCrawler;
+import com.mimosa.deeppokemon.entity.Battle;
+import com.mimosa.deeppokemon.entity.Player;
+import com.mimosa.deeppokemon.service.BattleService;
+import com.mimosa.deeppokemon.service.PlayerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class PokemoCrawlerApplication {
+    private static Logger log = LoggerFactory.getLogger(ScheduledConfig.class);
     public static void main(String[] args) {
         SpringApplication.run(PokemoCrawlerApplication.class, args);
+
     }
     @Bean
     LadderBattleCrawler crawler(){

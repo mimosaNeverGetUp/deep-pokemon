5ebdbb740cf16f2c9f95f8cb898c0a40de465762
mimosa <779032284@qq.com>
2024-04-02 16:28:52 +0800

    feat:BattleStat添加highLight

--- pokemon-front/src/components/BattleTable.vue
+++ pokemon-front/src/components/BattleTable.vue
@@ -1,4 +1,4 @@
-<script setup>
+                                                                                        <script setup>
 import {ref} from "vue";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";

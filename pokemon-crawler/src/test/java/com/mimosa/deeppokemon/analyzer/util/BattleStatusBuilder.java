9b5cea28a76d4c22ba98ff4e06d1467e51258f93
mimosa <779032284@qq.com>
2024-04-28 19:23:41 +0800

    feat: battle统计分析量化
    damage event analyzer增加分析钉子类伤害

---
+++ pokemon-crawler/src/test/java/com/mimosa/deeppokemon/analyzer/util/BattleStatusBuilder.java
@@ -0,0 +1,42 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2024-2024 mimosa
+ */
+
+package com.mimosa.deeppokemon.analyzer.util;
+
+import com.mimosa.deeppokemon.analyzer.entity.Side;
+import com.mimosa.deeppokemon.analyzer.entity.status.BattleStatus;
+import com.mimosa.deeppokemon.analyzer.entity.status.PlayerStatus;
+import com.mimosa.deeppokemon.analyzer.entity.status.PokemonStatus;
+
+import java.util.List;
+
+public class BattleStatusBuilder {
+    private List<PlayerStatus> playerStatuses;
+
+    public BattleStatusBuilder() {
+        playerStatuses = List.of(new PlayerStatus(), new PlayerStatus());
+    }
+
+    public BattleStatusBuilder addPokemon(int playerNumber, String pokemonName, String nickName) {
+        PlayerStatus playerStatus = playerStatuses.get(playerNumber - 1);
+        playerStatus.setPokemonStatus(pokemonName, new PokemonStatus(pokemonName));
+        playerStatus.setPokemonNickNameMap(nickName, pokemonName);
+        return this;
+    }
+
+    public BattleStatus build() {return new BattleStatus(playerStatuses);}
+
+    public BattleStatusBuilder addSide(int playerNumber, Side side) {
+        playerStatuses.get(playerNumber - 1).addSide(side);
+        return this;
+    }
+
+    public BattleStatusBuilder setTurnStartPokemon(int playerNumber, String pokemonName) {
+        playerStatuses.get(playerNumber - 1).setTurnStartPokemonName(pokemonName);
+        return this;
+    }
+
+}

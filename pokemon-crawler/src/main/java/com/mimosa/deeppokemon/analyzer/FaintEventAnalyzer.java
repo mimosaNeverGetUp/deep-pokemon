08110f016081c8185855846a4fc1a9b7a888d01f
mimosa <779032284@qq.com>
2024-06-13 10:39:42 +0800

    chore: add craw log

--- pokemon-crawler/src/main/java/com/mimosa/deeppokemon/analyzer/FaintEventAnalyzer.java
+++ pokemon-crawler/src/main/java/com/mimosa/deeppokemon/analyzer/FaintEventAnalyzer.java
@@ -41,7 +41,7 @@ public class FaintEventAnalyzer implements BattleEventAnalyzer {
             if (damageEventStat != null) {
                 EventTarget damageOf = damageEventStat.damageOf();
                 if (damageOf == null) {
-                    log.error("can't analyze damage with empty damageOf: {}, turn: ", damageEventStat,
+                    log.error("can't analyze damage with empty damageOf: {}, turn: {}", damageEventStat,
                             battleStatus.getTurn());
                     throw new RuntimeException("can't analyze damage with empty damageOf: " + damageEventStat);
                 }

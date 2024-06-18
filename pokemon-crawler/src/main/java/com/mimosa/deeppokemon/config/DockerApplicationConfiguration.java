29af28d681062f63ed16d96e4e14081f8c416b65
mimosa <779032284@qq.com>
2024-06-18 22:44:07 +0800

    feat: add swarm env profile

--- pokemon-crawler/src/main/java/com/mimosa/deeppokemon/config/DockerApplicationConfiguration.java
+++ pokemon-crawler/src/main/java/com/mimosa/deeppokemon/config/DockerApplicationConfiguration.java
@@ -7,6 +7,8 @@
 package com.mimosa.deeppokemon.config;
 
 import com.mongodb.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +19,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
+import java.util.Arrays;
 import java.util.Properties;
 
-@Profile("docker")
+@Profile({"docker", "swarm"})
 @Configuration
 public class DockerApplicationConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(DockerApplicationConfiguration.class);
     private static final String SPRING_DATA_MONGODB_URI = "spring.data.mongodb.uri";
     private static final String SPRING_DATA_MONGODB_DATABASE = "spring.data.mongodb.database";
     private Properties applicationProperties;
@@ -29,8 +33,17 @@ public class DockerApplicationConfiguration {
     private void initPropertiesIfNeed(ConfigurableEnvironment environment) {
         if (applicationProperties == null) {
             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-            factory.setResources(new ClassPathResource("application-docker.yml"));
+            if (Arrays.stream(environment.getActiveProfiles()).toList().contains("swarm")) {
+                log.info("use swarm application properties");
+                factory.setResources(new ClassPathResource("application-swarm.yml"));
+            } else {
+                factory.setResources(new ClassPathResource("application-docker.yml"));
+            }
             applicationProperties = factory.getObject();
+            if (applicationProperties == null) {
+                log.warn("no application properties found");
+                return;
+            }
             environment.getPropertySources().addFirst(new PropertiesPropertySource("docker", applicationProperties));
         }
     }

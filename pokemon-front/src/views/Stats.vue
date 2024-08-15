438834fffc459c7de5a6668e3969a4eb0f41df54
mimosa <779032284@qq.com>
2024-08-15 16:38:47 +0800

    feat: support select format

--- pokemon-front/src/views/Stats.vue
+++ pokemon-front/src/views/Stats.vue
@@ -8,22 +8,44 @@
 import StatsRank from '@/components/stats/StatsRank.vue'
 import PokemonStat from '@/components/stats/PokemonStat.vue';
 import MetaStat from '@/components/stats/MetaStat.vue';
+import TreeSelect from 'primevue/treeselect';
+
+import {formats} from "@/components/data/format.js";
 import {ref} from "vue";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 const selectPokemon = ref();
-const format = ref('gen9ou');
+const formatNodes = [];
+
+function getFormatNode() {
+  for (const formatKey in formats) {
+    formatNodes.push({
+      key: formatKey.split(".")[0],
+      label: formatKey.split(".")[0],
+      children: []
+    })
+  }
+}
 
 function updateSelectPokemon(pokemon) {
   selectPokemon.value = pokemon;
 }
-</script>
 
+async function onNodeSelect(event) {
+  await router.push({path: '/stats', query: {format: event.key}});
+  router.go(0);
+}
+
+getFormatNode();
+</script>
 <template>
-  <MetaStat :format="route.query.format" class="mt-[30px] mb-4"/>
+  <TreeSelect filter :options="formatNodes" :placeholder="route.query.format" class="mt-[30px]"
+              @node-select="onNodeSelect"/>
+  <MetaStat :format="route.query.format" class="mb-4"/>
   <div class="flex gap-2">
-    <StatsRank :updateSelectPokemon="updateSelectPokemon" :format="route.query.format "/>
-    <PokemonStat :pokemon="selectPokemon" :format="route.query.format "/>
+    <StatsRank :updateSelectPokemon="updateSelectPokemon" :format="route.query.format"/>
+    <PokemonStat :pokemon="selectPokemon" :format="route.query.format"/>
   </div>
 </template>

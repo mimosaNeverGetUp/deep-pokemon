8057b3047a650e55211ba7fb347f7dedf2cd72a5
mimosa <779032284@qq.com>
2024-08-22 23:18:52 +0800

    feat: load heat data

---
+++ pokemon-portal/src/main/java/com/mimosa/pokemon/portal/api/CacheApiController.java
@@ -0,0 +1,27 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2024-2024 mimosa
+ */
+
+package com.mimosa.pokemon.portal.api;
+
+import com.mimosa.pokemon.portal.service.CacheService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cache")
+public class CacheApiController {
+    private final CacheService cacheService;
+
+    public CacheApiController(CacheService cacheService) {
+        this.cacheService = cacheService;
+    }
+
+    @PostMapping("/load")
+    public boolean loadHeatData() {
+        return cacheService.loadHeatData();
+    }
+}

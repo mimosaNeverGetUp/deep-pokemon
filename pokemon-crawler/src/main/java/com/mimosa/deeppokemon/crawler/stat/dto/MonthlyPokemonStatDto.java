383431a646c209f4061e7f3d64b2257eb6d5102b
mimosa <779032284@qq.com>
2024-09-02 10:59:27 +0800

    feat: JsonAlias for tera types filed in stat

--- pokemon-crawler/src/main/java/com/mimosa/deeppokemon/crawler/stat/dto/MonthlyPokemonStatDto.java
+++ pokemon-crawler/src/main/java/com/mimosa/deeppokemon/crawler/stat/dto/MonthlyPokemonStatDto.java
@@ -6,12 +6,16 @@
 
 package com.mimosa.deeppokemon.crawler.stat.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import java.util.LinkedHashMap;
 import java.util.List;
 
 public record MonthlyPokemonStatDto(LeadStat lead, Usage usage, int count, double weight, List<Integer> viability,
                                     LinkedHashMap<String, Double> abilities, LinkedHashMap<String, Double> items,
                                     LinkedHashMap<String, Double> spreads, LinkedHashMap<String, Double> moves,
-                                    LinkedHashMap<String, Double> teraTypes, LinkedHashMap<String, Double> teammates,
-                                    LinkedHashMap<String, Double> happinesses, LinkedHashMap<String, List<Double>> counters) {
+                                    @JsonAlias({"teraTypes", "teraType", "teratypes", "Tera Types", "Tera Type",
+                                            "teras", "tera"}) LinkedHashMap<String, Double> teraTypes,
+                                    LinkedHashMap<String, Double> teammates, LinkedHashMap<String, Double> happinesses,
+                                    LinkedHashMap<String, List<Double>> counters) {
 }

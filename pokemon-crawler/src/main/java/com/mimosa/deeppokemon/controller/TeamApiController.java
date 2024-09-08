70a1319158e3a4f771463c8795fd3d8fd5dcaf34
mimosa <779032284@qq.com>
2024-09-08 20:04:32 +0800

    feat: team feature id

--- pokemon-crawler/src/main/java/com/mimosa/deeppokemon/controller/TeamApiController.java
+++ pokemon-crawler/src/main/java/com/mimosa/deeppokemon/controller/TeamApiController.java
@@ -7,6 +7,7 @@
 package com.mimosa.deeppokemon.controller;
 
 import com.mimosa.deeppokemon.entity.Tag;
+import com.mimosa.deeppokemon.migrate.BattleTeamMigrator;
 import com.mimosa.deeppokemon.service.TeamService;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +17,23 @@ import java.util.Set;
 @RequestMapping("/api")
 public class TeamApiController {
     private final TeamService teamService;
+    private final BattleTeamMigrator battleTeamMigrator;
 
-    public TeamApiController(TeamService teamService) {
+    public TeamApiController(TeamService teamService, BattleTeamMigrator battleTeamMigrator) {
         this.teamService = teamService;
+        this.battleTeamMigrator = battleTeamMigrator;
     }
 
     @GetMapping("/team/tag")
     public Set<Tag> updateTeam(@RequestParam("teamId") String teamId,
                                @RequestParam("collectionName") String collectionName) {
         return teamService.tagTeam(teamId, collectionName);
+    }
+
+
+    @PostMapping("/team/migrate")
+    public String updateTeamFeatureId() {
+        battleTeamMigrator.migrateBattleTeam();
+        return "success";
     }
 }

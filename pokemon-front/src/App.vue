4cdd85cb11701de6052a0322f171b803564c4526
mimosa <779032284@qq.com>
2024-09-26 11:35:46 +0800

    front: add footer

--- pokemon-front/src/App.vue
+++ pokemon-front/src/App.vue
@@ -1,14 +1,37 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import Divider from "primevue/divider";
 
 </script>
 
 <template>
   <Header class="text-lg" show-update-date="true">
   </Header>
-  <router-view></router-view>
+  <div class="main">
+    <router-view></router-view>
+    <Divider type="solid"/>
+    <footer class="footer">
+      <span class="text-gray-600">© 2024 Mimosa</span>
+      <a target="_blank" :href="`https://space.bilibili.com/37832209`" >
+        <img src="/bilibili.svg" alt="bilibili" class="size-5">
+      </a>
+    </footer>
+  </div>
+
 </template>
 
 <style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
+.footer {
+  margin-top: auto;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  gap: 0.25rem;
+}
 </style>

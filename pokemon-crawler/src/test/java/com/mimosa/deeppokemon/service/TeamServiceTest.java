e966609308fb1f6038cd7ea1df88d9beaedbc381
mimosa <779032284@qq.com>
2025-01-08 23:27:29 +0800

    chore: clean code

--- pokemon-crawler/src/test/java/com/mimosa/deeppokemon/service/TeamServiceTest.java
+++ pokemon-crawler/src/test/java/com/mimosa/deeppokemon/service/TeamServiceTest.java
@@ -14,7 +14,6 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;

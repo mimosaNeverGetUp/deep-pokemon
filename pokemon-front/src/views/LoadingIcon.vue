b7279816ee2bc9e6286d977d710a7d026c74b3a0
mimosa <779032284@qq.com>
2025-09-25 18:21:11 +0800

    feat: better loading icon

--- pokemon-front/src/views/LoadingIcon.vue
+++ pokemon-front/src/views/LoadingIcon.vue
@@ -6,22 +6,31 @@
 <script setup>
 function getRandomGif() {
   let random = Math.random();
-  if (random <= 0.33) {
-    return "/gif/clefable.gif";
-  } else if (random <= 0.66) {
-    return "/gif/blissey.gif";
-  } else {
-    return "/gif/alcremie-caramel-swirl-love.gif";
-  }
-
+  // if (random <= 0.33) {
+  //   return "/gif/clefable.gif";
+  // } else if (random <= 0.66) {
+  //   return "/gif/blissey.gif";
+  // } else {
+  //   return "/gif/alcremie-caramel-swirl-love.gif";
+  // }
+  return "/gif/alcremie-caramel-swirl-love.gif";
 }
 </script>
 
 <template>
   <div class="m-auto flex flex-col items-center justify-center">
     <div>
-      <img :src="getRandomGif()" alt="loading...">
+      <img :src="getRandomGif()" alt="loading..." class="loadingIcon bg-gray-200 p-3">
     </div>
     <p class="font-sans">{{$t('loading')}}</p>
   </div>
 </template>
+
+<style>
+.loadingIcon {
+  border: 1px solid;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+}
+</style>
